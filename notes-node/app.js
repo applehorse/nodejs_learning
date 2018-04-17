2db02c154fc81271e64d824d24074311774fec76
William Ma <william.ma@spidertracks.co.nz>
2018-04-17 16:48:58 +1200

    import yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,14 +1,24 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
 
-var user = os.userInfo();
-//console.log(user);
+const notes = require('./notes.js');
 
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
 
-fs.appendFile('greetings.txt',`Hello ${user.username}!`, function(err) {
-    if (err) {
-        console.log('Unable to write to file');
-    }
-});
+if (command === 'add'){
+    notes.addNote(argv.title, argv.body);
+} else if(command === 'list'){
+    notes.getAll();
+} else if(command === 'read'){
+    notes.getNote(argv.title, argv.body);
+} else if(command === 'remove'){
+    notes.removeNote(argv.title);
+} else {
+    console.log('Commnad not recognized');
+}
